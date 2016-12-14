cd3976f112a2ce98d07f0bf5d69af23bb637d42b
Baptiste Pouget <baba@firemail.cc>
2016-12-14 21:35:42 +0100

    Added basic bonusÂ

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ const string KCyan    ("36");
 
 typedef vector <char> CVLine; // un type représentant une ligne de la grille
 typedef vector <CVLine> CMatrix; // un type représentant la grille
-typedef pair   <unsigned, unsigned> CPosition; // un type représentant une coordonnée dans la grille
 const char KEmpty = '-';
 typedef struct {
     unsigned posX;
@@ -26,6 +25,16 @@ typedef struct {
     char token;
     string color;
 } Player;
+
+void Couleur (const string & coul);
+void ClearScreen ();
+void ShowMatrix (const CMatrix & Mat,Player & player1, Player & player2);
+void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & player1, Player & player2);
+void MoveToken (CMatrix & Mat, char Move, Player & player);
+void GetBonus(CMatrix & Mat, Player & player);
+void PutBonus(CMatrix & Mat, const char & token, const unsigned & posX, const unsigned & posY);
+bool CheckWin(Player & player1, Player & player2);
+int ppal();
 
 void Couleur (const string & coul)
 {
@@ -54,7 +63,8 @@ void ShowMatrix (const CMatrix & Mat,Player & player1, Player & player2)
     }
 }
 
-void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & player1, Player & player2){
+void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & player1, Player & player2)
+{
     Mat.resize(NbLine);
     for (unsigned i (0); i<NbLine; ++i)
     {
@@ -63,8 +73,12 @@ void InitMat (CMatrix & Mat, unsigned NbLine, unsigned NbColumn, Player & player
             Mat[i].push_back(KEmpty);
         }
     }
-    Mat[player1.posY][player1.posX] = player1.token;
-    Mat[player2.posY][player2.posX] = player2.token;
+    for (unsigned i (player1.posY); i<player1.posY + player1.sizeY; ++i)
+        for (unsigned j (player1.posX); j<player1.posX + player1.sizeX; ++j)
+            Mat[i][j] = player1.token;
+    for (unsigned i (player2.posY); i<player2.posY + player2.sizeY; ++i)
+        for (unsigned j (player2.posX); j<player2.posX + player2.sizeX; ++j)
+            Mat[i][j] = player2.token;
 }
 
 void MoveToken (CMatrix & Mat, char Move, Player & player)
@@ -74,38 +88,80 @@ void MoveToken (CMatrix & Mat, char Move, Player & player)
     case 'z':
         if (player.posY > 0)
         {
-        Mat[player.posY-1][player.posX] = player.token;
-        Mat[player.posY][player.posX] = KEmpty;
-        player.posY = player.posY - 1;
+            player.posY = player.posY - 1;
+            GetBonus(Mat, player);
+            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
+                Mat[player.posY + player.sizeY][i] = KEmpty;
+            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
+                Mat[player.posY][i] = player.token;
         }
     break;
     case 's':
-        if (player.posY < Mat.size()-1)
-        {
-        Mat[player.posY+1][player.posX] = player.token;
-        Mat[player.posY][player.posX] = KEmpty;
-        player.posY = player.posY + 1;
+        if (player.posY + player.sizeY < Mat.size())
+        {            
+            player.posY = player.posY + 1;
+            GetBonus(Mat, player);
+            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
+                Mat[player.posY - 1][i] = KEmpty;
+            for (unsigned i (player.posX); i < player.posX + player.sizeX; ++i)
+                Mat[player.posY + player.sizeY - 1][i] = player.token;
 
         }
     break;
     case 'q':
         if (player.posX > 0)
         {
-        Mat[player.posY][player.posX-1] = player.token;
-        Mat[player.posY][player.posX] = KEmpty;
-        player.posX = player.posX - 1;
-
+            player.posX = player.posX - 1;
+            GetBonus(Mat, player);
+            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
+                Mat[i][player.posX + player.sizeX] = KEmpty;
+            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
+                Mat[i][player.posX] = player.token;
         }
     break;
     case 'd':
-        if (player.posX < Mat[0].size()-1)
+        if (player.posX + player.sizeX < Mat[0].size())
         {
-        Mat[player.posY][player.posX+1] = player.token;
-        Mat[player.posY][player.posX] = KEmpty;
-        player.posX = player.posX + 1;
+            player.posX = player.posX + 1;
+            GetBonus(Mat, player);
+            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
+                Mat[i][player.posX - 1] = KEmpty;
+            for (unsigned i (player.posY); i < player.posY + player.sizeY; ++i)
+                Mat[i][player.posX + player.sizeX - 1] = player.token;
         }
     break;
     }
+}
+
+void PutBonus(CMatrix & Mat, const char & token, const unsigned & posX, const unsigned & posY)
+{
+    Mat[posY][posX] = token;
+}
+
+void GetBonus(CMatrix & Mat, Player & player)
+{
+    for (unsigned i (player.posY); i<player.posY + player.sizeY; ++i)
+    {
+        for (unsigned j (player.posX); j<player.posX + player.sizeX; ++j)
+        {
+            if (Mat[i][j] == 'B')
+            {
+                ++player.sizeX;
+                ++player.sizeY;
+                for (unsigned i (player.posY); i<player.posY + player.sizeY; ++i)
+                    for (unsigned j (player.posX); j<player.posX + player.sizeX; ++j)
+                        Mat[i][j] = player.token;
+            }
+        }
+    }
+}
+
+bool CheckWin(Player & player1, Player & player2)
+{
+    return !((player1.posX > player2.posX + player2.sizeX - 1) ||
+            (player1.posX + player1.sizeX - 1 < player2.posX) ||
+            (player2.posY > player1.posY + player1.sizeY - 1)  ||
+            (player2.posY + player2.sizeY - 1 < player1.posY));
 }
 
 int ppal ()
@@ -131,6 +187,7 @@ int ppal ()
     char Mv;
     CMatrix Map;
     InitMat(Map,KSizeX,KSizeY,player1,player2);
+    PutBonus(Map,'B',2,2);
     ShowMatrix(Map,player1,player2);
     cout << player1.token << " commence et chasse " << player2.token << endl;
     for (unsigned i (0); i < NbRnds*2; ++i)
@@ -140,7 +197,7 @@ int ppal ()
         cout << "Au tour de " << (i%2 == 0 ? player1.token : player2.token) << endl;
         cin >> Mv;
         MoveToken (Map, Mv, (i%2 == 0 ? player1 : player2) );
-        if ((player1.posX == player2.posX)&&(player1.posY==player2.posY))
+        if (CheckWin(player1,player2))
         {
             cout << (i%2 == 0 ? player1.token : player2.token) << " a gagne !" << endl;
             return 0;
